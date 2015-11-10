d402ac4e7ec7e8128c390b90e5f306606da01b69
Artyom <noname_nv@mail.ru>
2015-11-10 23:19:58 +0500

    Добавлен класс, который составляет рейтинг записей.

---
+++ PostsRating/PostsRating/Controller/PostsRatingMaker.cs
@@ -0,0 +1,68 @@
+﻿using PostsRating.Controller.Request.Sender;
+using PostsRating.Model;
+using System;
+using System.Collections.Generic;
+
+namespace PostsRating.Controller
+{
+    class PostsRating
+    {
+        private User targetUser;
+        private FriendsRequest vkFriends;
+        private WallRequest vkWall;
+        private UserRequest vkUser;
+        private List<Post> topPosts;
+        private int timeToLive = (int)(DateTime.Now.AddDays(-7) - new DateTime(1970, 1, 1)).TotalSeconds;
+
+        public PostsRating()
+        {
+            vkFriends = new FriendsRequest();
+            vkWall    = new WallRequest();
+            vkUser    = new UserRequest();
+        }
+        // Составить рейтинг из 10 самых популярных записей
+        public void toRankPosts(string linkToUser)
+        {
+            targetUser = new User(linkToUser);
+            if (targetUser == null) { return; }
+            setFriendsToUser();
+            setPostsToUsersFriends();
+        }
+        // Получить список друзей для целевого пользователя
+        private void setFriendsToUser()
+        {
+            List<string> friendsList = vkFriends.getUsersFriends(targetUser);
+            if (friendsList.Count > 0)
+            {
+                foreach (string friend in friendsList)
+                {
+                    targetUser.addFriend(friend);
+                }
+            }
+        }
+        //  Получить список записей за прошедшую неделю для каждого пользователя в списке друзей
+        private void setPostsToUsersFriends()
+        {
+            List<User> usersList = targetUser.friends;
+            if (usersList != null && usersList.Count > 0)
+            {
+                List<string> postsList;
+                foreach (User friend in usersList)
+                {
+                    postsList = vkWall.getFriendsPosts(friend, timeToLive);
+                    if (postsList != null && postsList.Count > 0)
+                    {
+                        foreach (string id in postsList)
+                        {
+                            friend.addPost(id);
+                        }
+                    }
+                } // foreach (User friend)
+            } // if (userList)
+        }
+        private void getLikesForPosts()
+        {
+
+        }
+    }
+}
--- PostsRating/PostsRating/Controller/UserLinkParser.cs
+++ PostsRating/PostsRating/Controller/UserLinkParser.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace PostsRating.Controller
 {
     class UserLinkParser
     {
+
+        private static string serverPattern = "^(https?:\\/\\/)?vk.com\\/";
+        private static string idPattern = "^(https?:\\/\\/)?vk.com\\/id[1-9]{1}[0-9]{0,9}";
+        private static string nickPattern = "^(https?:\\/\\/)?vk.com\\/[a-z]{1}[a-z0-9-_]{2,16}";
+
+        private static bool checkLinkForId(string linkToUser)
+        {
+            return Regex.IsMatch(linkToUser, idPattern, RegexOptions.IgnoreCase);
+        }
+        private static bool checkLinkForNick(string linkToUser)
+        {
+            return Regex.IsMatch(linkToUser, nickPattern, RegexOptions.IgnoreCase);
+        }
+        public static string parseLink(string linkToUser)
+        {
+            if (checkLinkForId(linkToUser))
+            {
+                return Regex.Replace(linkToUser, serverPattern += "id", "");
+            }
+            if (checkLinkForNick(linkToUser))
+            {
+                return Regex.Replace(linkToUser, serverPattern, "");
+            }
+            return string.Empty;
+        }
     }
 }
