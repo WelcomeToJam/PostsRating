645c89513dbe2b1fc97aaeec3c6d1825fe2d57d8
Artyom <noname_nv@mail.ru>
2015-11-10 22:32:48 +0500

    Добавлены классы моделей

---
+++ PostsRating/PostsRating/Model/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostsRating.Model
+{
+    class Post
+    {
+        public string ownerId { get; private set; }
+        public string id      { get; private set; }
+        public int likesCount { get; private set; }
+        public Post(string id, string ownerId)
+        {
+            this.id = id;
+            this.ownerId = ownerId;
+        }
+    }
+}
---
+++ PostsRating/PostsRating/Model/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostsRating.Model
+{
+    class User
+    {
+        public string id { get; private set; }
+        public string firstName { get; private set; }
+        public string lastName { get; private set; }
+        public List<User> friends { get; private set; }
+        public List<Post> posts { get; private set; }
+
+        public User(string id)
+        {
+            this.id = id;
+            friends = new List<User>();
+            posts = new List<Post>();
+        }
+        public void addFriend(string friendId)
+        {
+            friends.Add(new User(friendId));
+        }
+        public void addPost(string postId)
+        {
+            posts.Add(new Post(postId, id));
+        }
+    }
+
+}
+}
