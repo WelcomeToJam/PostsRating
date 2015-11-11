9e1ccd40e87acdbd7db4960ac315552aab02f48d
Artyom <noname_nv@mail.ru>
2015-11-11 23:31:19 +0500

    Добавлены комментарии.

--- PostsRating/PostsRating/Controller/UserLinkParser.cs
+++ PostsRating/PostsRating/Controller/UserLinkParser.cs
@@ -2,21 +2,25 @@
 
 namespace PostsRating.Controller
 {
+    // Класс проверяющий корректность пользовательской ссылки 
     class UserLinkParser
     {
-
         private static string serverPattern = "^(https?:\\/\\/)?vk.com\\/";
         private static string idPattern = "^(https?:\\/\\/)?vk.com\\/id[1-9]{1}[0-9]{0,9}";
         private static string nickPattern = "^(https?:\\/\\/)?vk.com\\/[a-z]{1}[a-z0-9-_]{2,16}";
 
-        private static bool checkLinkForId(string linkToUser)
+        // Метод, возвращающий true, если указан id пользователя.
+        public static bool checkLinkForId(string linkToUser)
         {
             return Regex.IsMatch(linkToUser, idPattern, RegexOptions.IgnoreCase);
         }
-        private static bool checkLinkForNick(string linkToUser)
+        // Метод, возвращающий true, если указан nickname пользователя.
+        public static bool checkLinkForNick(string linkToUser)
         {
             return Regex.IsMatch(linkToUser, nickPattern, RegexOptions.IgnoreCase);
         }
+        // Метод, возвращающий значение id или nickname пользователя.
+        // При отсутсвии совпадений возвращает пустую строку.
         public static string parseLink(string linkToUser)
         {
             if (checkLinkForId(linkToUser))
